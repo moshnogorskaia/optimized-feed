db88e7f9ef8cd7165f66b442ebc974df2d71350e
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-26 09:11:45 +0100

    Add priority to the image

--- components/header.js
+++ components/header.js
@@ -6,7 +6,13 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo} width={100} height={100} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          priority
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
@@ -14,7 +20,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
